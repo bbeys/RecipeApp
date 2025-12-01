a72042e4fdc48b670df173d1defce32e5b4d6d59
pipipopipoopi <po.she@icloud.com>
2025-12-01 02:39:05 +0000

    Update Recipio.sql

--- Recipio.sql
+++ Recipio.sql
@@ -39,7 +39,6 @@ CREATE TABLE `ADMIN` (
 --
 
 INSERT INTO `ADMIN` (`admin_id`, `email`, `password_hash`, `created_at`) VALUES
-(1, 'admin@recipeapp.com', 'recipioadmin', '2025-11-30 20:04:58'),
 (2, 'priya@example.com', 'adminpass', '2025-11-30 20:23:09');
 
 -- --------------------------------------------------------
