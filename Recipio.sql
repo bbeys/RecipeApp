5297d622ea6b1967bb72737158981ab88845862f
bbeys <beyzasaglam07@gmail.com>
2025-12-01 17:40:19 +0000

    Fix admin user queries to extract name from email since ADMIN table has no first_name/last_name columns

--- Recipio.sql
+++ Recipio.sql
@@ -29,6 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `ADMIN` (
   `admin_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password_hash` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
